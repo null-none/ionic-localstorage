d906a8f4e9170211e95d7616cd5de5e077ac7267
Dmitry <dmitry.kalinin.email@gmail.com>
2016-09-02 10:00:21 +0300

    version 0.0.2

--- ionicLocalStorage.js
+++ ionicLocalStorage.js
@@ -1,18 +1,25 @@
 angular.module('ionicLocalStorage', [])
 
-.service('ionicLocalStoragenService', function($window){
-  return {
-    set: function(key, value) {
-      $window.localStorage[key] = value;
-    },
-    get: function(key, defaultValue) {
-      return $window.localStorage[key] || defaultValue;
-    },
-    setObject: function(key, value) {
-      $window.localStorage[key] = JSON.stringify(value);
-    },
-    getObject: function(key) {
-      return JSON.parse($window.localStorage[key] || '{}');
+.service('ionicLocalStoragenService', function($window) {
+
+    this.set = function(key, value) {
+        $window.localStorage[key] = value;
     }
-  }
+
+    this.get = function(key, defaultValue) {
+        return $window.localStorage[key] || defaultValue;
+    }
+
+    this.setObject = function(key, value) {
+        $window.localStorage[key] = JSON.stringify(value);
+    }
+
+    this.getObject = function(key) {
+        return JSON.parse($window.localStorage[key] || '{}');
+    }
+
+    this.clear = function() {
+        $window.localStorage.clear()
+    }
+}
 });
